85751db73158c10ff86727ffd81e38d1e4fdada4
radekska <radoslaw.skalbania@gmail.com>
2022-07-20 21:29:55 +0200

    work on coffee machine (stage 4)

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	waterPerCoffee = 200
+	milkPerCoffee  = 50
+	beansPerCoffee = 15
+)
 
 type Ingredients struct {
 	water       int
@@ -8,11 +17,15 @@ type Ingredients struct {
 	coffeeBeans int
 }
 
-const (
-	waterPerCoffee = 200
-	milkPerCoffee  = 50
-	beansPerCoffee = 15
-)
+type CoffeeMachineSupplies struct {
+	ingredients Ingredients
+	cups        int
+	money       int
+}
+
+func NewCoffeeMachineSupplies(ingredients Ingredients, cups int, money int) CoffeeMachineSupplies {
+	return CoffeeMachineSupplies{ingredients: ingredients, cups: cups, money: money}
+}
 
 func NewIngredients(water int, milk int, coffeeBeans int) Ingredients {
 	return Ingredients{
@@ -39,39 +52,86 @@ func calculateAvailableCoffees(currentIngredients Ingredients) int {
 	return minimal
 }
 
-func getInput(value int) int {
+func getInput(value int) {
 	_, err := fmt.Scan(&value)
 	if err != nil {
 		fmt.Println(err)
 	}
-	return value
+}
+
+func getCoffeeMachineState(coffeeMachineSupplies CoffeeMachineSupplies) string {
+	var messages = []string{"The coffee machine has:", "%d ml of water", "%d ml of milk", "%d g of coffee beans",
+		"%d disposable cups", "$%d of money"}
+	message := strings.Join(messages, "\n")
+	return fmt.Sprintf(message, coffeeMachineSupplies.ingredients.water, coffeeMachineSupplies.ingredients.milk,
+		coffeeMachineSupplies.ingredients.coffeeBeans, coffeeMachineSupplies.cups, coffeeMachineSupplies.money)
+}
+
+const (
+	espresso   = iota
+	latte      = iota
+	cappuccino = iota
+)
+
+func makeCoffee(coffeeMachineSupplies *CoffeeMachineSupplies, option int) {
+	switch option {
+	case espresso:
+		coffeeMachineSupplies.ingredients.water -= 250
+		coffeeMachineSupplies.ingredients.coffeeBeans -= 16
+		coffeeMachineSupplies.money += 4
+	case latte:
+		coffeeMachineSupplies.ingredients.water -= 350
+		coffeeMachineSupplies.ingredients.milk -= 75
+		coffeeMachineSupplies.ingredients.coffeeBeans -= 20
+		coffeeMachineSupplies.money += 7
+	case cappuccino:
+		coffeeMachineSupplies.ingredients.water -= 200
+		coffeeMachineSupplies.ingredients.milk -= 100
+		coffeeMachineSupplies.ingredients.coffeeBeans -= 12
+		coffeeMachineSupplies.money += 6
+	}
+	if option == espresso || option == latte || option == cappuccino {
+		coffeeMachineSupplies.cups -= 1
+	}
+
+}
+
+func fillCoffeeMachineSupplies(coffeeMachineSupplies *CoffeeMachineSupplies, water int, milk int, coffeeBeans int, cups int) {
+	coffeeMachineSupplies.ingredients.water += water
+	coffeeMachineSupplies.ingredients.milk += milk
+	coffeeMachineSupplies.ingredients.coffeeBeans += coffeeBeans
+	coffeeMachineSupplies.cups += cups
+}
+
+func takeMoneyFromCoffeeMachine(coffeeMachineSupplies *CoffeeMachineSupplies, money int) {
+	if coffeeMachineSupplies.money >= money {
+		coffeeMachineSupplies.money -= money
+	}
+}
+
+const (
+	buy  = "buy"
+	fill = "fill"
+	take = "take"
+)
+
+func writeAction(coffeeMachineSupplies *CoffeeMachineSupplies) {
+	fmt.Println("Write action (buy, fill, take):")
+	var action string
+	fmt.Scanf("%s", &action)
+
+	switch action {
+	case buy:
+		fmt.Println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+		var coffee int
+		fmt.Scanf("%d", &coffee)
+		makeCoffee(coffeeMachineSupplies, coffee)
+	}
 }
 
 func main() {
-	var neededCups, currentWater, currentMilk, currentBeans int
-	fmt.Print("Write how many ml of water the coffee machine has:\n> ")
-	currentWater = getInput(currentWater)
-
-	fmt.Print("Write how many ml of milk the coffee machine has:\n> ")
-	currentMilk = getInput(currentMilk)
-
-	fmt.Print("Write how many grams of coffee beans the coffee machine has:\n> ")
-	currentBeans = getInput(currentBeans)
-
-	fmt.Print("Write how many cups of coffee you will need:\n> ")
-	neededCups = getInput(neededCups)
-
-	var currentIngredients = NewIngredients(currentWater, currentMilk, currentBeans)
-	var availableCups = calculateAvailableCoffees(currentIngredients)
-
-	if availableCups == neededCups {
-		fmt.Println("Yes, I can make that amount of coffee")
-	}
-	if availableCups < neededCups {
-		fmt.Printf("No, I can make only %d cups of coffee\n", availableCups)
-	}
-	if availableCups > neededCups {
-		extraCups := availableCups - neededCups
-		fmt.Printf("Yes, I can make that amount of coffee (and even %d more than that)\n", extraCups)
-	}
+	coffeeMachineSupplies := NewCoffeeMachineSupplies(NewIngredients(400, 540, 120), 9, 550)
+	fmt.Println(getCoffeeMachineState(coffeeMachineSupplies))
+	writeAction(&coffeeMachineSupplies)
+	fmt.Println(getCoffeeMachineState(coffeeMachineSupplies))
 }
--- main_test.go
+++ main_test.go
@@ -13,3 +13,42 @@ func TestCalculateAvailableCoffees(t *testing.T) {
 
 	assert.Equal(t, expectedCoffees, availableCoffees, "Should be equal.")
 }
+
+func TestGetCoffeeMachineState(t *testing.T) {
+	coffeeMachineSupplies := NewCoffeeMachineSupplies(NewIngredients(100, 50, 10), 5, 1000)
+
+	message := getCoffeeMachineState(coffeeMachineSupplies)
+
+	assert.Equal(t, "The coffee machine has:\n100 ml of water\n50 ml of milk\n10 g of coffee beans\n5 disposable cups\n$1000 of money", message)
+}
+
+func TestMakeCoffee(t *testing.T) {
+	coffeeMachineSupplies := NewCoffeeMachineSupplies(NewIngredients(1000, 100, 50), 5, 1000)
+
+	makeCoffee(&coffeeMachineSupplies, espresso)
+
+	assert.Equal(t, 750, coffeeMachineSupplies.ingredients.water)
+	assert.Equal(t, 34, coffeeMachineSupplies.ingredients.coffeeBeans)
+	assert.Equal(t, 1004, coffeeMachineSupplies.money)
+	assert.Equal(t, 4, coffeeMachineSupplies.cups)
+
+}
+
+func TestFillCoffeeMachine(t *testing.T) {
+	coffeeMachineSupplies := NewCoffeeMachineSupplies(NewIngredients(1000, 100, 50), 5, 1000)
+
+	fillCoffeeMachineSupplies(&coffeeMachineSupplies, 500, 250, 10, 30)
+
+	assert.Equal(t, 1500, coffeeMachineSupplies.ingredients.water)
+	assert.Equal(t, 350, coffeeMachineSupplies.ingredients.milk)
+	assert.Equal(t, 60, coffeeMachineSupplies.ingredients.coffeeBeans)
+	assert.Equal(t, 35, coffeeMachineSupplies.cups)
+}
+
+func TestTakeMoneyFromCoffeeMachine(t *testing.T) {
+	coffeeMachineSupplies := NewCoffeeMachineSupplies(NewIngredients(1000, 100, 50), 5, 1000)
+
+	takeMoneyFromCoffeeMachine(&coffeeMachineSupplies, 1000)
+
+	assert.Equal(t, 0, coffeeMachineSupplies.money)
+}
